5781470724335124e092da2b7c0e0297ae7d44f7
Nemo D'ACREMONT <ndacremont@enseirb-matmeca.fr>
2025-05-20 10:39:20 +0200

    feat: create a server class in the lib

--- common/src/server.ts
+++ common/src/server.ts
@@ -1,10 +1,47 @@
 import { Socket} from 'dgram'
+import { EventEmitter } from 'stream'
+import { createSocket } from 'dgram';
+import { Client } from './client';
 
 type Response = (message: Buffer) => Promise<void>
 
-export class Server extends Socket {
+export interface ServerEvents {
+        'connect': [];
+        'close': [];
+        'listening': [];
+        'error': [Error];
+        'message': [msg: Buffer, {send: (msg: string) => void}];
+}
 
-    onMessage(callback: (data: Buffer, res: Response) => void) {}
+export class Server extends EventEmitter<ServerEvents> {
+    private socket;
+    private host;
+    private port;
 
-    bind(host: string, port: number) {}
+    constructor (host: string, port: number) {
+        super();
+        this.socket = createSocket('udp4');
+        this.host = host;
+        this.port = port;
+
+        this.socket.on('connect', () => this.emit('connect'));
+        this.socket.on('listening', () => this.emit('listening'));
+        this.socket.on('error', (err) => this.emit('error', err));
+        this.socket.on('close', () => this.emit('close'));
+
+        this.socket.on('message', (msg, rinfo) => {
+            const client = createSocket('udp4');
+
+            this.emit('message', msg, {
+                send: (msg: string) => {
+                    const data = Buffer.from(msg);
+                    client.send(data, rinfo.port, rinfo.address)
+                },
+            });
+        });
+    }
+
+    bind () {
+        this.socket.bind(this.port, this.host);
+    }
 }
