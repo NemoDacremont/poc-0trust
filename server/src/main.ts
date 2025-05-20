3b252ad56f7731c6ade27d1444be0e935c6efc09
Nemo D'ACREMONT <ndacremont@enseirb-matmeca.fr>
2025-05-20 16:55:35 +0200

    fix: use properly the getPSKById

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,4 +1,4 @@
-import { KeyPair } from "../../common/src";
+import { Key, KeyPair } from "../../common/src";
 import { Server } from "../../common/src/server";
 
 const s = KeyPair.fromPrivate(
@@ -7,7 +7,17 @@ const s = KeyPair.fromPrivate(
     "hex",
   ),
 );
-const server = new Server("localhost", 12345, s);
+
+// Fake function
+const clients: Record<string, Key> = {
+  e51eaac8fe8aac9d95b3a7d215b330fb1b09bafc54625c22e76656190d98b246: Buffer.from(
+    "9775b0f777581be4c9922c7b001ec9ce01f044216b4a30d63b5a5b411300c68b",
+    "hex",
+  ),
+};
+const getPSKbyID = (id: Key): Key | null => clients[id.toString("hex")] ?? null;
+
+const server = new Server("localhost", 12345, s, getPSKbyID);
 
 server.on("message", (msg, client) => {
   console.log(`Received : ${msg}`);
