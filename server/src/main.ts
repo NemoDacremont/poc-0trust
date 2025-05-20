aa722d0e44efd3d7c8dfc690b35689200015ad7f
Nemo D'ACREMONT <ndacremont@enseirb-matmeca.fr>
2025-05-20 15:28:27 +0200

    update: update echo server using the final config

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,6 +1,13 @@
+import { KeyPair } from "../../common/src";
 import { Server } from "../../common/src/server";
 
-const server = new Server("localhost", 12345);
+const s = KeyPair.fromPrivate(
+  Buffer.from(
+    "126e5c21772c68e1398cc48c0880f349fb410cdcca953c89a84cdba17fd9df72",
+    "hex",
+  ),
+);
+const server = new Server("localhost", 12345, s);
 
 server.on("message", (msg, client) => {
   console.log(`Received : ${msg}`);
