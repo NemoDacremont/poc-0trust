fc6550de0e281fa864d1ac4570b7fc815a31946f
Screamnox <92693756+Screamnox@users.noreply.github.com>
2025-05-21 16:53:58 +0200

    refactor: rm 2nd test

--- common/tst/KeyPair.test.ts
+++ common/tst/KeyPair.test.ts
@@ -1,4 +1,3 @@
-import { randomInt } from "crypto";
 import { KeyPair } from "../src";
 import { KEY_SIZE } from "../src/noise-spa/constants";
 
@@ -7,12 +6,4 @@ test("KeyPair.generate should generate KEY_SIZE long keys by default", () => {
 
   expect(keyPair.getPublic().length).toBe(KEY_SIZE);
   expect(keyPair.getPrivate().length).toBe(KEY_SIZE);
-});
-
-test("KeyPair.generate should generate keySize long keys", () => {
-  const keySize = randomInt(128);
-  const keyPair = KeyPair.generate(keySize);
-
-  expect(keyPair.getPublic().length).toBe(keySize);
-  expect(keyPair.getPrivate().length).toBe(keySize);
 });
