b8992fc475588a881c195e9991e6115de1868406
Nemo D'ACREMONT <ndacremont@enseirb-matmeca.fr>
2025-05-21 16:38:40 +0200

    feat: use asserts for precond in keypair

--- common/src/noise-spa/KeyPair.ts
+++ common/src/noise-spa/KeyPair.ts
@@ -2,22 +2,38 @@ import { randomBytes } from "crypto";
 import { Key } from "./utils";
 import { x25519 } from "@noble/curves/ed25519";
 import { KEY_SIZE } from "./constants";
+import assert from "assert";
+import { InvalidKeySizeError } from "./exceptions/InvalidKeySizeError";
 
 export class KeyPair {
   private public: Key;
   private private: Key;
 
-  static generate(keySize = KEY_SIZE): KeyPair {
-    const privateKey = randomBytes(keySize);
+  static generate(): KeyPair {
+    const privateKey = randomBytes(KEY_SIZE);
     return KeyPair.fromPrivate(privateKey);
   }
 
   static fromPrivate(privateKey: Key): KeyPair {
+    assert(
+      privateKey.byteLength === KEY_SIZE,
+      new InvalidKeySizeError(KEY_SIZE, privateKey.byteLength),
+    );
+
     const publicKey = Buffer.from(x25519.getPublicKey(privateKey));
     return new KeyPair(publicKey, privateKey);
   }
 
   constructor(publicKey: Key, privateKey: Key) {
+    assert(
+      publicKey.byteLength === KEY_SIZE,
+      new InvalidKeySizeError(KEY_SIZE, publicKey.byteLength),
+    );
+    assert(
+      privateKey.byteLength === KEY_SIZE,
+      new InvalidKeySizeError(KEY_SIZE, privateKey.byteLength),
+    );
+
     this.public = publicKey;
     this.private = privateKey;
   }
